92f47196ae848007b3675aed2b7f5974e50d54f6
alex1234567639 <alex8603062000@gmail.com>
2020-08-22 16:43:15 +0800

    gallary

--- dest/js/landingPage.js
+++ dest/js/landingPage.js
@@ -9,4 +9,13 @@ $(document).ready(function() {
         return false;
     });
 
+    $("#gallery").click(function() {
+      $("html, body").animate({
+        scrollTop: $(".contentBtn2nd").offset().top
+      }, {
+        duration: 800,
+        easing: "swing"
+      });
+      return false;
+  });
 });
